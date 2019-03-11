52bfd2775169dcb253933c10d19a2002173be073
markvan <mvanmiddle@gmail.com>
2019-03-10 21:44:55 -0400

    Add kiosk mode shell script

---
+++ kiosk.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Launch a browser in kiosk mode on display 0
+# Based on https://www.sylvaindurand.org/launch-chromium-in-kiosk-mode/
+export DISPLAY=:0
+
+xset -dpms
+xset s off
+xset s noblank
+
+
+unclutter &
+chromium-browser 127.0.0.1:5000/chart --window-size=320,240 --start-fullscreen --kiosk --incognito \
+    --noerrdialogs --disable-translate --no-first-run --fast --fast-start --disable-infobars \
+    --disable-features=TranslateUI --disk-cache-dir=/dev/null
